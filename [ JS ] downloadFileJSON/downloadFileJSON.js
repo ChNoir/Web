429c176e27d160cbfaf70784e860482f525dfb6c
ChNoir <94179306+ChNoir@users.noreply.github.com>
2022-01-05 22:02:18 +0100

    Update downloadFileJSON.js

--- [ JS ] downloadFileJSON/downloadFileJSON.js
+++ [ JS ] downloadFileJSON/downloadFileJSON.js
@@ -6,7 +6,7 @@ function downloadFileJSON( Data = { test: "ok" } , namefile = "test" ) {
     // il faut rajoute <a id = "downloadAnchorElem" style = "display : none;"></a> 
     // pour que la Function marche !!! 1 sufit pour tout 
     // on peux utilise un label pour apple la function
-    // <label onclick="downloadFileJSON(data,'test')">downloadFileJSON</la
+    // <label onclick="downloadFileJSON(downloadData,'test')">downloadFileJSON</la
     //
 
     var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(Data));
