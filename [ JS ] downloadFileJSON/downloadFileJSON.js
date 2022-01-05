a4668735664f33af1a4f2c180a04d8bc6ccde612
ChNoir <94179306+ChNoir@users.noreply.github.com>
2022-01-05 21:22:32 +0100

    Add files via upload

---
+++ [ JS ] downloadFileJSON/downloadFileJSON.js
@@ -0,0 +1,15 @@
+
+function downloadFileJSON( Data = { test: "ok" } , namefile = "test" ) {
+
+    //
+    // il faut rajoute <a id = "downloadAnchorElem" style = "display : none;"></a> 
+    // pour que la Function marche !!! 1 sufit pour tout 
+    //
+
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(Data));
+    var dlAnchorElem = document.getElementById("downloadAnchorElem");
+    dlAnchorElem.setAttribute("href",     dataStr     );
+    dlAnchorElem.setAttribute("download", ( namefile + ".json"));
+    // simule un click sur l'element pour lanche le telecharge a chaque fois qua la fuction est apple
+    dlAnchorElem.click();
+}
---
+++ [ JS ] uploadFileJSON/uploadFileJSON.js
@@ -0,0 +1,32 @@
+
+var Data ;
+
+function onChange(event) {
+    console.log(data)
+    var reader = new FileReader();
+    reader.onload = onReaderLoad;
+    reader.readAsText(event.target.files[0]);
+    
+}
+
+function onReaderLoad(event){
+    Data = JSON.parse(event.target.result);
+    document.getElementById('uploadJSON').value = "";
+    // console.log(Data);
+}
+
+// Dans window.onload pour definire l'événement de chargement du fichie JSON
+document.getElementById('uploadJSON').addEventListener('change', onChange);
+
+
+// Dans le HTML 
+// au moment ou on click sur les label, il demande la l'utilisateur un fichie JSON 
+// puis il est recuperer dans la variable Data
+// il y a besion d'un seul input pour tou 
+//
+// <input type="file" id="uploadJSON" accept=".json" style="display : none;"></input>
+// <label for="uploadJSON" >lol</label>
+//
+
+
+// ce programme peut être utilisé si les modifier.
